49bdb3b7a9bbe64efbf9f207e56d0a830ee92637
Ivaan-github <85596537+Ivaan-github@users.noreply.github.com>
2021-06-13 20:44:42 -0500

    Added GithubDemo Class

---
+++ Ram test/src/practice/GithubDemo.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class GithubDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("To Verify GitHub commits after adding to the repository");
+
+	}
+
+}
