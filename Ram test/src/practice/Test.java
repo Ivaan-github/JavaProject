7339ccf6fc8309b2817a79f841022bb179813d25
Ivaan-github <85596537+Ivaan-github@users.noreply.github.com>
2021-06-13 21:01:03 -0500

    changed Test class after integrated with Azure

--- Ram test/src/practice/Test.java
+++ Ram test/src/practice/Test.java
@@ -5,9 +5,12 @@ package practice;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("This is one more github commit in existing class");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println(c);
 
 	}
 
